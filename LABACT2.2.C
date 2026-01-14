b6cb3165650be29b0b9caffc4675f3f992f027ee
elli-0550 <ellainecastro139@gmail.com>
2026-01-14 16:28:44 +0000

    NPP

--- LABACT2.2.C
+++ LABACT2.2.C
@@ -3,56 +3,55 @@
 
 int main()
 {
-    int bt[20], wt[20], tat[20], i, n, quantum, remaining[20];
+     int bt[20], wt[20], tat[20], priority[20], i, n, temp;
     float wtavg = 0, tatavg = 0;
   
-    printf("\nEnter the number of processes -- ");
+     printf("\nEnter the number of processes -- ");
     scanf("%d", &n);
     for(i=0; i<n; i++)
     {
         printf("\nEnter Burst Time for Process %d -- ", i);
         scanf("%d", &bt[i]);
-        remaining[i] = bt[i]; // Copy burst times to remaining array
+        printf("Enter Priority for Process %d (lower number = higher priority) -- ", i);
+        scanf("%d", &priority[i]);
     }
-    printf("\nEnter Time Quantum -- ");
-    scanf("%d", &quantum);
     
-    // Simulate Round Robin Scheduling
-    int time = 0; // Current time
-    int done = 0; // Number of processes completed
-    while(done < n)
+    // Sort processes by priority (non-preemptive: higher priority first)
+    // Assuming lower priority number means higher priority
+    for(i=0; i<n-1; i++)
     {
-        for(i=0; i<n; i++)
+        for(int j=i+1; j<n; j++)
         {
-            if(remaining[i] > 0)
+            if(priority[i] > priority[j])
             {
-                if(remaining[i] > quantum)
-                {
-                    time += quantum;
-                    remaining[i] -= quantum;
-                }
-                else
-                {
-                    time += remaining[i];
-                    wt[i] = time - bt[i]; // Waiting time = Completion time - Burst time
-                    remaining[i] = 0;
-                    done++;
-                }
+                // Swap burst times
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+                // Swap priorities
+                temp = priority[i];
+                priority[i] = priority[j];
+                priority[j] = temp;
             }
         }
     }
     
-    // Calculate Turnaround Time and averages
-    for(i=0; i<n; i++)
+    // Calculate waiting and turnaround times
+    wt[0] = 0; // First process waits 0
+    tat[0] = bt[0];
+    for(i=1; i<n; i++)
     {
-        tat[i] = bt[i] + wt[i];
+        wt[i] = wt[i-1] + bt[i-1];
+        tat[i] = tat[i-1] + bt[i];
         wtavg += wt[i];
         tatavg += tat[i];
     }
+    wtavg += wt[0]; // Add first process
+    tatavg += tat[0];
     
-    printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
+    printf("\t PROCESS \tBURST TIME \t PRIORITY \t WAITING TIME\t TURNAROUND TIME\n");
     for(i=0; i<n; i++)
-        printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
+        printf("\n\t P%d \t\t %d \t\t %d \t\t %d \t\t %d", i, bt[i], priority[i], wt[i], tat[i]);
     printf("\nAverage Waiting Time -- %f", wtavg/n);
     printf("\nAverage Turnaround Time -- %f", tatavg/n);
    return 0;
